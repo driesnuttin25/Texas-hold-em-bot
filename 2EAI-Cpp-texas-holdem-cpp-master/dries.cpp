9469c9714c69d79ea847c81733dddbc704c24c96
Dries Nuttin <dries.nuttin@hotmail.com>
2023-10-15 20:16:58 +0200

    Couple changes to code

--- 2EAI-Cpp-texas-holdem-cpp-master/dries.cpp
+++ 2EAI-Cpp-texas-holdem-cpp-master/dries.cpp
@@ -3,7 +3,7 @@
 #include <cmath> // for abs()
 #include "game.h"
 
-namespace PXL2022
+namespace PXL2023
 {
 
 const char* Dries::getName() const
@@ -17,79 +17,68 @@ int Dries::willYouRaise(unsigned int totalBet)
     unsigned int blind = getGame()->getBlind();
     unsigned int myChips = getChips();
 
-    // Randomizer
-    int randomFactor = rand() % 10; // Generate random number between 0 and 9
+    // Improved Randomizer
+    int randomFactor = rand() % 20;
+
+    // Basic fold conditions - don't waste chips
+    if (myChips < blind * 2)
+    {
+        return -1;
+    }
 
     if (getTable()->isPreFlop()) // Pre-flop
     {
         auto firstCard = getHand().getFirstCard();
         auto secondCard = getHand().getSecondCard();
 
-        // High Pairs (10 and above) - Aggressive strategy
+        unsigned int baseRaise = blind * 4;
+
         if (firstCard->getRank() == secondCard->getRank() && firstCard->getRank() >= 10)
         {
-            // Adding randomization to occasionally call instead of raising
-            return (randomFactor > 1) ? (blind * 5) - totalBet : (blind * 4) - totalBet;
+            return baseRaise + blind;
         }
 
-        // Suited Cards - Pot building strategy
         if (firstCard->getSuit() == secondCard->getSuit())
         {
-            // Consider stack size
-            if (myChips >= blind * 10) {
-                return (blind * 3) - totalBet;
-            }
+            return baseRaise;
         }
 
-        // Connected Cards - Conditional strategy
         if (abs(firstCard->getRank() - secondCard->getRank()) == 1)
         {
-            // Add opponent behavior later (for now, raise 2x blind)
-            return (blind * 2) - totalBet;
+            return baseRaise - blind;
         }
 
-        // Modified folding strategy based on hand strength
-        if (totalBet > blind * 4)
+        if (firstCard->getRank() < 8 && secondCard->getRank() < 8)
         {
-            PokerRank myHandRank = getRank(); // Getting your hand rank
-
-            if (myHandRank.getCategory() == HIGH_CARD || myHandRank.getCategory() == ONE_PAIR && myHandRank.getRank() < 10) {
-                return -1; // Fold if you have a high card or a low pair
-            }
+            return -1;
         }
 
-
-        // Unpredictable play, occasionally call or make a small raise with less optimal hands
-        if (randomFactor == 0 && myChips >= blind * 5) {
-            return (blind * 2) - totalBet;
+        if (randomFactor == 0 && myChips >= blind * 5)
+        {
+            return baseRaise + blind * 2;
         }
     }
 
-    else if (getTable()->isFlop())
+    else if (getTable()->isFlop() || getTable()->isTurn() || getTable()->isRiver())
     {
-        PokerRank myHandRank = getRank();
-        unsigned int blind = getGame()->getBlind();
-        unsigned int myChips = getChips();
+        unsigned int myCurrentBet = getBet();
 
-        if (myHandRank.getCategory() == ONE_PAIR)
+        switch (myHandRank.getCategory())
         {
-            return (blind * 2) - totalBet; // Raise 2x blind if you have a pair
-        }
-        else if (myHandRank.getCategory() == TWO_PAIR)
-        {
-            return (blind * 4) - totalBet; // Raise 4x blind if you have two pair
-        }
-        else if (myHandRank.getCategory() == THREE_OF_A_KIND)
-        {
-            return (blind * 6) - totalBet; // Raise 6x blind if you have three of a kind
-        }
-        else if (myHandRank.getCategory() == STRAIGHT)
-        {
-            return (blind * 5) - totalBet; // Raise 5x blind if you have a straight
-        }
-        else
-        {
-            return 0; // Check or Call
+        case ONE_PAIR:
+            return (totalBet - myCurrentBet < blind * 3) ? 0 : blind * 2;
+        case TWO_PAIR:
+            return blind * 4;
+        case THREE_OF_A_KIND:
+            return blind * 5;
+        case STRAIGHT:
+            return blind * 6;
+        default:
+            if (totalBet - myCurrentBet > myChips * 0.2)
+            {
+                return -1; // Fold
+            }
+            return 0; // Call
         }
     }
 
--- 2EAI-Cpp-texas-holdem-cpp-master/dries.h
+++ 2EAI-Cpp-texas-holdem-cpp-master/dries.h
@@ -3,7 +3,7 @@
 #include "player.h"
 
 
-namespace PXL2022
+namespace PXL2023
 {
 class Dries : public Player
 {
